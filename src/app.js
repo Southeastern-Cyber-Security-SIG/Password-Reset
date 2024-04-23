a2149d99dcd3d8da04e3208fddfd6cf7fefd3e14
Joshua Cantu <joshua@thecantu.com>
2024-04-22 21:33:38 -0500

    Added routers

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
+var resetRouter = require('./routes/reset');
 var usersRouter = require('./routes/users');
+var seedHelper = require('./seedHelper');
+seedHelper.seed();
 
 var app = express();
 
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +20,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/reset', resetRouter);
 app.use('/users', usersRouter);
+
 
 module.exports = app;

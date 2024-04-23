41341b75a3b6a1a98339058f37ceb7f34f431d7b
Joshua Cantu <joshua@thecantu.com>
2024-04-22 23:46:12 -0500

    The stuffs

--- src/routes/reset.js
+++ src/routes/reset.js
@@ -1,5 +1,25 @@
 var express = require('express');
+var validator = require('validator');
+
+var dbHelper = require('../dbHelper');
+
 var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('reset/reset', { status: '' });
+  });
+
+  router.post('/', function(req, res, next) {
+    if(validator.isEmail(req.body.email)){
+        result = dbHelper.getQuestion(email)
+        
+        res.render('reset/reset', { status: 'Done' });
+
+    }else{
+        res.render('reset/reset', { status: 'Enter a Vaild Email' });
+    }
+   
+  });
 
 
 module.exports = router;

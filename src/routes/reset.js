75a796711182d903bce7f612f05b70c21333715f
Joshua Cantu <joshua@thecantu.com>
2024-04-23 12:38:44 -0500

    Made some stuff async

--- src/routes/reset.js
+++ src/routes/reset.js
@@ -9,11 +9,14 @@ router.get('/', function(req, res, next) {
     res.render('reset/reset', { status: '' });
   });
 
-  router.post('/', function(req, res, next) {
+  router.post('/', async function(req, res, next) {
     if(validator.isEmail(req.body.email)){
-        result = dbHelper.getQuestion(email)
-        
-        res.render('reset/reset', { status: 'Done' });
+        result = dbHelper.getQuestion(req.body.email)
+        console.log(result)
+        if(result == "[]"){
+          res.render('reset/reset', { status: 'Email Found' });
+        }
+        res.render('reset/reset', { status: 'Email not Found' });
 
     }else{
         res.render('reset/reset', { status: 'Enter a Vaild Email' });

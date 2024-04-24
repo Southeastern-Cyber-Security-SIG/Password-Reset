0372a19b910b5541f7338c8c50dd86a90a998870
Joshua Cantu <joshua@thecantu.com>
2024-04-23 21:08:04 -0500

    Password reset is complete

--- src/dbHelper.js
+++ src/dbHelper.js
@@ -1,78 +1,112 @@
 var mysql = require('mysql2');
 
 var con = mysql.createConnection({
-        host: "localhost",
-        user: "MYSQL_USER",
-        password: "MYSQL_PASSWORD",
-        database: "mydatabase"
-      });
+  host: "localhost",
+  user: "MYSQL_USER",
+  password: "MYSQL_PASSWORD",
+  database: "mydatabase"
+});
 
 function seed() {
-      con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-
-        var sql = "CREATE TABLE IF NOT EXISTS users (`email` varchar(255) PRIMARY KEY, `password` varchar(225), `name` varchar(225), `authToken` varchar(225), `question` varchar(225), `answer` varchar(225), `flag` varchar(225))";
-        con.query(sql, function (err) {
-          if (err) throw err;
-          console.log("Table created");
-        });
-
-        var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('joshua@cssig.selu.edu', 'Joshua Cantu', '00IY5IlTLPaK', 'What was your first pets name?', 'Norway', 'selu{I_<3_P3ts!_P3opleSuck}');";
-        con.query(sql, function (err) {
-          if (err) throw err;
-          console.log("Joshua's User Created");
-        });
-        
-        var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('kody@cssig.selu.edu', 'Kody Chabaud', '6ErN07an7S8Q', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
-        con.query(sql, function (err) {
-          if (err) throw err;
-          console.log("Kody's User Created");
-        });
-
-        var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('tevor@cssig.selu.edu', 'Trevor Patorno', 'xkIlCmYTnzQp', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
-        con.query(sql, function (err) {
-          if (err) throw err;
-          console.log("Trevor's User Created");
-        });
-
-        var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('shanna@cssig.selu.edu', 'Shanna OQuinn', 'lfBLcSxocbgh', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
-        con.query(sql, function (err) {
-          if (err) throw err;
-          console.log("Shanna's User Created");
-        });
-
-        var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('patrick@cssig.selu.edu', 'Patrick Theroit', 'KeDdBnYiKlLo', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
-        con.query(sql, function (err) {
-          if (err) throw err;
-          console.log("Patrick's User Created");
-        });
-
-        var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('caz@cssig.selu.edu', 'Caz Cheramie', 'ymlPTGDWyGoe', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
-        con.query(sql, function (err) {
-          if (err) throw err;
-          console.log("Caz's User Created");
-        });
-
-      });
-    console.log("Done Seeding")
-}
-
- async function getQuestion(email){
-  con.connect(function(err) {
+  con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 
-    var sql = "SELECT question FROM users WHERE email = '"+email+"';";
-    con.query(sql, async function (err, result, fields) {
+    var sql = "CREATE TABLE IF NOT EXISTS users (`email` varchar(255) PRIMARY KEY, `password` varchar(225), `name` varchar(225), `authToken` varchar(225), `question` varchar(225), `answer` varchar(225), `flag` varchar(225))";
+    con.query(sql, function (err) {
       if (err) throw err;
-      console.log(sql)
-      console.log(result) ;
-      return await result
+      console.log("Table created");
     });
-  
 
+    var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('joshua@cssig.selu.edu', 'Joshua Cantu', '00IY5IlTLPaK', 'What was your first pets name?', 'Norway', 'selu{I_<3_P3ts!_P3opleSuck}');";
+    con.query(sql, function (err) {
+      if (err) throw err;
+      console.log("Joshua's User Created");
+    });
+
+    var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('kody@cssig.selu.edu', 'Kody Chabaud', '6ErN07an7S8Q', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
+    con.query(sql, function (err) {
+      if (err) throw err;
+      console.log("Kody's User Created");
+    });
+
+    var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('tevor@cssig.selu.edu', 'Trevor Patorno', 'xkIlCmYTnzQp', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
+    con.query(sql, function (err) {
+      if (err) throw err;
+      console.log("Trevor's User Created");
+    });
+
+    var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('shanna@cssig.selu.edu', 'Shanna OQuinn', 'lfBLcSxocbgh', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
+    con.query(sql, function (err) {
+      if (err) throw err;
+      console.log("Shanna's User Created");
+    });
+
+    var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('patrick@cssig.selu.edu', 'Patrick Theroit', 'KeDdBnYiKlLo', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
+    con.query(sql, function (err) {
+      if (err) throw err;
+      console.log("Patrick's User Created");
+    });
+
+    var sql = "INSERT IGNORE INTO users (email, name, authToken, question, answer, flag) VALUES ('caz@cssig.selu.edu', 'Caz Cheramie', 'ymlPTGDWyGoe', 'Whats your mothers maiden name?', 'Smith', 'selu{M0ms_@re_Th3_Besst!}');";
+    con.query(sql, function (err) {
+      if (err) throw err;
+      console.log("Caz's User Created");
+    });
 
   });
+  console.log("Done Seeding")
 }
-module.exports = { seed, getQuestion };
+
+async function getQuestion(email) {
+  return await new Promise((resolve, reject) => {
+    con.connect(function (err) {
+      if (err) throw err;
+      console.log("Connected!");
+
+      var sql = "SELECT question FROM users WHERE email = '" + email + "';";
+      con.query(sql, async function (err, result, fields) {
+        if (err) reject(err);
+        console.log(sql)
+        console.log(result);
+        resolve(result)
+      });
+    })
+  });
+}
+
+async function checkQuestion(email, answer) {
+  return await new Promise((resolve, reject) => {
+    con.connect(function (err) {
+      if (err) throw err;
+      console.log("Connected!");
+
+      var sql = "SELECT email FROM users WHERE email = '" + email + "' AND answer = '"+ answer+"';";
+      con.query(sql, async function (err, result, fields) {
+        if (err) reject(err);
+        console.log(sql)
+        console.log(result);
+        resolve(result)
+      });
+    })
+  });
+}
+
+async function resetPassword(email, answer, password) {
+  return await new Promise((resolve, reject) => {
+    con.connect(function (err) {
+      if (err) throw err;
+      console.log("Connected!");
+
+      var sql = "UPDATE users Set password = '"+password+"' WHERE email = '"+email+"' AND answer ='"+answer+"';";
+      con.query(sql, async function (err, result, fields) {
+        if (err) reject(err);
+        console.log(sql)
+        console.log(result);
+        resolve(result)
+      });
+    })
+  });
+}
+
+module.exports = { seed, getQuestion, checkQuestion, resetPassword };
--- src/routes/reset.js
+++ src/routes/reset.js
@@ -5,24 +5,61 @@ var dbHelper = require('../dbHelper');
 
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('reset/reset', { status: '' });
-  });
+router.get('/', function (req, res, next) {
+  res.render('reset/reset', { status: '' });
+});
 
-  router.post('/', async function(req, res, next) {
-    if(validator.isEmail(req.body.email)){
-        result = dbHelper.getQuestion(req.body.email)
-        console.log(result)
-        if(result == "[]"){
-          res.render('reset/reset', { status: 'Email Found' });
-        }
-        res.render('reset/reset', { status: 'Email not Found' });
-
-    }else{
-        res.render('reset/reset', { status: 'Enter a Vaild Email' });
+router.post('/', async function (req, res, next) {
+  if (validator.isEmail(req.body.email)) {
+    result = await dbHelper.getQuestion(req.body.email)
+    console.log(result)
+    if (result && result.length == 0) {
+      console.log("Email not found")
+      res.render('reset/reset', { status: 'Email not Found' });
+    } else {
+      console.log("Email found")
+      res.render('reset/question', { status: '  ', email: req.body.email, question: result[0].question });
     }
+  } else {
+    res.render('reset/reset', { status: 'Enter a Valid Email' });
+  }
+
+});
+
+router.post('/question', async function (req, res, next) {
+  console.log(req.body)
+  if (validator.isEmail(req.body.email) && validator.isAlphanumeric(req.body.answer)) {
+    result = await dbHelper.checkQuestion(req.body.email, req.body.answer)
+
+    console.log(result)
+    if (result && result.length == 0) {
+      console.log("Wrong Answer")
+      res.render('reset/question', { status: 'Wrong Answer', email: req.body.email, question:  req.body.question });
+    } else {
+      console.log("Correct Answer")
+      res.render('reset/password', { status: ' ', email: req.body.email, answer: req.body.answer });
+    }
+  } else {
+    res.render('reset/question', { status: ' Something is Wrong  ', email: req.body.email, question: req.body.question });
+  }
+
+});
+
+router.post('/password', async function (req, res, next) {
+  console.log(req.body)
+  if (validator.isEmail(req.body.email) && validator.isAlphanumeric(req.body.answer) && validator.isAlphanumeric(req.body.password)) {
+    result = await dbHelper.resetPassword(req.body.email, req.body.answer, req.body.password)
+
+    console.log(result)
    
-  });
+    res.render('login', { status: 'Password has been reset' });
+    
+
+  } else {
+    res.render('reset/password', { status: ' Something is Wrong  ', email: req.body.email, question: req.body.question });
+  }
+
+});
 
 
 module.exports = router;
